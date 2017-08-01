56aebbe065fc65b752bd464f0781b40205926ee8
Vishal Dandge <vishaldandge1@gmail.com>
2017-08-01 12:53:29 +0530

    Initial Commit

---
+++ Picazo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Picazo
+//
+//  Created by Vishal Dandge on 01/08/17.
+//  Copyright © 2017 Plado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
